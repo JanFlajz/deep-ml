7c31b425706fb1821172da820bc1f327fe455316
JanFlajz <62598168+JanFlajz@users.noreply.github.com>
2026-09-18 12:23:09 +0000

    Solve: Matrix times Matrix

---
+++ problems/0009-matrix-times-matrix/solution.py
@@ -0,0 +1,9 @@
+import numpy as np
+def matrixmul(a:list[list[int|float]],
+              b:list[list[int|float]])-> list[list[int|float]]:
+
+    A = np.array(a)
+    B = np.array(b)
+    if A.shape[1] != B.shape[0]:
+        return -1
+	return (A @ B).tolist()
