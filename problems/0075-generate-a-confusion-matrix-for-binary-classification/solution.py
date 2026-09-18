8d0880f804c541935682a961444b021800c80183
JanFlajz <62598168+JanFlajz@users.noreply.github.com>
2026-09-18 09:36:57 +0000

    Update: Generate a Confusion Matrix for Binary Classification

--- problems/0075-generate-a-confusion-matrix-for-binary-classification/solution.py
+++ problems/0075-generate-a-confusion-matrix-for-binary-classification/solution.py
@@ -3,18 +3,11 @@ from collections import Counter
 
 def confusion_matrix(data):
 	# Implement the function here
-	true_pos = false_pos = false_neg = true_neg = 0
-	for tup in data:
-		if tup[0]:
-			if tup[1]:
-				true_pos += 1
-			else:
-				false_neg += 1
-		else:
-			if tup[1]:
-				false_pos += 1
-			else:
-				true_neg += 1
+	true_pos = sum(x and y for x,y in data)
+	false_pos = sum(not x and y for x,y in data)
+	false_neg = sum(x and not y for x,y in data)
+	true_neg = sum(not x and not y for x,y in data)
+
 			 
 
 	return [[true_pos,false_neg],[false_pos,true_neg]]
